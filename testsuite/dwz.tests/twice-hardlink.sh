019b628a8072c089db92e6af685a6b8ffd33d0d1
Tom de Vries <tdevries@suse.de>
2019-03-17 16:11:38 +0100

    Add twice-hardlink.sh test-case
    Add a test-case that tests that hardlinks are maintained when files cannot be
    compressed further.
    
    2019-03-17  Tom de Vries  <tdevries@suse.de>
    
    	* testsuite/dwz.tests/twice-hardlink.sh: New test.

---
+++ testsuite/dwz.tests/twice-hardlink.sh
@@ -0,0 +1,36 @@
+cp ../hello 1
+ln 1 2
+
+dwz -h 1 2
+
+smaller-than.sh 1 ../hello
+
+hardlinks-p.sh 1 2
+
+cp 1 1.saved
+
+if dwz -h 1 2 2>dwz.err; status=$?; then
+    true
+fi
+
+if ! grep -q "DWARF compression not beneficial" dwz.err; then
+    cat dwz.err
+    exit 1
+fi
+
+if [ $(grep -qv "DWARF compression not beneficial" dwz.err \
+	   | wc -l) -gt 0 ]; then
+    cat dwz.err
+    exit 1
+fi
+
+[ $status -eq 0 ]
+
+cmp 1 1.saved
+
+rm -f 1 1.saved 2 2.saved dwz.err
+
+if [ -f 2.#dwz#.* ]; then
+    echo "PR24275 workaround used" > dwz.info
+    rm -f 2.#dwz#.*
+fi
