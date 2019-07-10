e179a15d4e8e7bf10c8bf9735443f7adba98563d
Tom de Vries <tdevries@suse.de>
2019-07-10 21:31:16 +0200

    Handle section overlap from objcopy --only-keep-debug
    On debian 7 for MIPS, I run into:
    ...
    dwz: Section overlap detected
    dwz: Section header table: [0x628, 0xcb8)
    dwz: Section 26: 0x824
    FAIL: testsuite/dwz.tests/objcopy-eu-unstrip.sh
    ...
    which is caused by running dwz with as argument an executable obtained using
    objcopy --only-keep-debug.
    
    Fix this by marking the test-case unsupported if dwz bails out with "Section
    overlap detected" error.
    
    2019-07-10  Tom de Vries  <tdevries@suse.de>
    
    	* testsuite/dwz.tests/objcopy-eu-unstrip-multifile.sh: Mark
    	unsupportedif dwz bails out with "Section overlap detected" error.
    	* testsuite/dwz.tests/objcopy-eu-unstrip.sh:

--- testsuite/dwz.tests/objcopy-eu-unstrip-multifile.sh
+++ testsuite/dwz.tests/objcopy-eu-unstrip-multifile.sh
@@ -5,12 +5,20 @@ objcopy --strip-debug 1 1.stripped
 
 cp 1.debug 2.debug
 
-dwz -m 3 1.debug 2.debug
+if dwz -m 3 1.debug 2.debug 2> dwz.err; status=$?; then
+    true
+fi
 
 rm 2.debug
+
+if grep -q "dwz: Section overlap detected" dwz.err; then
+    exit 77
+fi
+
+[ $status -eq 0 ]
 
 eu-unstrip 1.stripped 1.debug -o 1.unstripped
 
 smaller-than.sh 1.unstripped 1
 
-rm -f 1 1.debug 1.stripped 1.unstripped 3
+rm -f 1 1.debug 1.stripped 1.unstripped 3 dwz.err
--- testsuite/dwz.tests/objcopy-eu-unstrip.sh
+++ testsuite/dwz.tests/objcopy-eu-unstrip.sh
@@ -3,10 +3,18 @@ cp ../hello 1
 objcopy --only-keep-debug 1 1.debug
 objcopy --strip-debug 1 1.stripped
 
-dwz 1.debug
+if dwz 1.debug 2> dwz.err; status=$?; then
+    true
+fi
+
+if grep -q "dwz: Section overlap detected" dwz.err; then
+    exit 77
+fi
+
+[ $status -eq 0 ]
 
 eu-unstrip 1.stripped 1.debug -o 1.unstripped
 
 smaller-than.sh 1.unstripped 1
 
-rm -f 1 1.debug 1.stripped 1.unstripped
+rm -f 1 1.debug 1.stripped 1.unstripped dwz.err
