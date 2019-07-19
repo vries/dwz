6bfe45ef37831d8c299746e78a8e16103ded6893
Tom de Vries <tdevries@suse.de>
2019-07-19 14:07:05 +0200

    Require single pointer size in pr24823.sh
    On MIPS we run into:
    ...
    dwz: 1: DWARF pointer size differs between CUs
    dwz: 2: DWARF pointer size differs between CUs
    dwz: Too few files for multifile optimization
    FAIL: testsuite/dwz.tests/pr24823.sh
    ...
    
    Fix this by requiring a single pointer size for exec varval in pr24823.sh.
    
    2019-07-19  Tom de Vries  <tdevries@suse.de>
    
    	* testsuite/dwz.tests/pr24823.sh: Require single pointer size.

--- testsuite/dwz.tests/pr24823.sh
+++ testsuite/dwz.tests/pr24823.sh
@@ -1,5 +1,14 @@
 exec=$execs/varval
 
+pointer_sizes=$(readelf -wi $exec \
+		    | grep "Pointer Size:" \
+		    | sort -u \
+		    | wc -l)
+
+if [ $pointer_sizes -ne 1 ]; then
+    exit 77
+fi
+
 cp $exec 1
 cp 1 2
 
