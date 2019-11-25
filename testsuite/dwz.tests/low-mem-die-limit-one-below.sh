af33bb8c1bb5774493bba69030a9f6474f7aa09c
Tom de Vries <tdevries@suse.de>
2019-11-25 10:42:55 +0100

    Add low-mem-die-limit-{one-above,one-below,at-limit}.sh test-cases
    Add test-cases that test the low-mem limit:
    - one below the die count
    - one above the die count
    - equal to the die count
    
    2019-11-25  Tom de Vries  <tdevries@suse.de>
    
    	* testsuite/dwz.tests/low-mem-die-limit-at-limit.sh: New test.
    	* testsuite/dwz.tests/low-mem-die-limit-one-above.sh: New test.
    	* testsuite/dwz.tests/low-mem-die-limit-one-below.sh: New test.

---
+++ testsuite/dwz.tests/low-mem-die-limit-one-below.sh
@@ -0,0 +1,18 @@
+cp $execs/hello 1
+
+count=$(readelf -wi 1 \
+	    | grep '(DW_TAG' \
+	    | wc -l)
+limit=$((count - 1))
+
+$execs/dwz-for-test \
+    -l$limit \
+    --devel-trace \
+    1 \
+    2> dwz.err
+
+if ! grep -q "Compressing 1 in low-mem mode" dwz.err; then
+    exit 1
+fi
+
+rm -f 1 dwz.err
