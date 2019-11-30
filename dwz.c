vries/dwz

commit 6eedbd72cea06c35af814d4cbfb8a882f764f763

Dump more information with --devel-dump-dies
For --devel-dump-dies, additionally dump the tag (without the DW_TAG_ prefix
for brevity) as well as the DW_AT_type attribute of a DIE:
...
-  88 O b500801d ccbaf67d
+  88 O b500801d ccbaf67d  pointer_type (type: 8e char base_type)
...

2019-11-30  Tom de Vries  <tdevries@suse.de>

	* dwz.c (dump_type): New function.
	(dump_die_with_indent): Print tag and type.

dwz.c
@@ -4128,15 +4128,48 @@ find_dups_fi (dw_die_ref parent)
   return 0;
 }
 
+/* Dump type of DIE to stderr.  */
+static void
+dump_type (dw_die_ref die)
+{
+  bool present;
+  enum dwarf_form form;
+  unsigned int value = get_AT_int (die, DW_AT_type, &present, &form);
+  if (!present)
+    return;
+
+  dw_cu_ref cu = die_cu (die);
+  if (cu == NULL)
+    return;
+
+  dw_die_ref ref;
+  if (form != DW_FORM_ref_addr)
+    value = cu->cu_offset + value;
+  fprintf (stderr, " (type: %x", value);
+  ref = off_htab_lookup (cu, value);
+  if (ref != NULL)
+    {
+      const char *type_name = get_AT_string (ref, DW_AT_name);
+      if (type_name)
+	fprintf (stderr, " %s", type_name);
+    }
+  fprintf (stderr, " %s", get_DW_TAG_name (ref->die_tag) + 7);
+  dump_type (ref);
+  fprintf (stderr, ")");
+}
+
 /* Dump DIE to stderr with INDENT.  */
 static void
 dump_die_with_indent (int indent, dw_die_ref die)
 {
   const char *name = get_AT_string (die, DW_AT_name);
-  fprintf (stderr, "%*s %x %c %x %x %s\n", indent, "", die->die_offset,
+  fprintf (stderr, "%*s %x %c %x %x %s %s", indent, "", die->die_offset,
 	   die->die_ck_state == CK_KNOWN ? 'O' : 'X',
 	   (unsigned) die->u.p1.die_hash,
-	   (unsigned) die->u.p1.die_ref_hash, name ? name : "");
+	   (unsigned) die->u.p1.die_ref_hash, name ? name : "",
+	   get_DW_TAG_name (die->die_tag) + 7);
+  dump_type (die);
+  fprintf (stderr, "\n");
 }
 
 /* Dump DIE tree at tree depth DEPTH.  */

commit e2e183ca8d9b655619e84f9a0d5c315aff772ce9

Fix dump_type and dump_die_with_indent in low-mem mode
When using --devel-dump-dies in combination with low-mem mode, we run into a
segfault:
...
$ dwz cc1 --devel-dump-dies -l0 2>/dev/null
Segmentation fault (core dumped)
...
which is a regression caused by commit 6eedbd7 "Dump more information with
--devel-dump-dies".

The segfault is caused by trying to access invalidated fields in a DIE with
die_collapsed_child set.

Fix this by handling die_collapsed_child == 1 in dump_type and
dump_die_with_indent.

2019-11-30  Tom de Vries  <tdevries@suse.de>

	* dwz.c (dump_type, dump_die_with_indent): Handle
	die_collapsed_child == 1.

dwz.c
@@ -4136,6 +4136,8 @@ dump_type (dw_die_ref die)
 {
   bool present;
   enum dwarf_form form;
+  if (die->die_collapsed_child)
+    return;
   unsigned int value = get_AT_int (die, DW_AT_type, &present, &form);
   if (!present)
     return;
@@ -4149,14 +4151,14 @@ dump_type (dw_die_ref die)
     value = cu->cu_offset + value;
   fprintf (stderr, " (type: %x", value);
   ref = off_htab_lookup (cu, value);
-  if (ref != NULL)
+  if (ref != NULL && !ref->die_collapsed_child)
     {
       const char *type_name = get_AT_string (ref, DW_AT_name);
       if (type_name)
 	fprintf (stderr, " %s", type_name);
+      fprintf (stderr, " %s", get_DW_TAG_name (ref->die_tag) + 7);
+      dump_type (ref);
     }
-  fprintf (stderr, " %s", get_DW_TAG_name (ref->die_tag) + 7);
-  dump_type (ref);
   fprintf (stderr, ")");
 }
 
@@ -4164,13 +4166,21 @@ dump_type (dw_die_ref die)
 static void
 dump_die_with_indent (int indent, dw_die_ref die)
 {
-  const char *name = get_AT_string (die, DW_AT_name);
-  fprintf (stderr, "%*s %x %c %x %x %s %s", indent, "", die->die_offset,
-	   die->die_ck_state == CK_KNOWN ? 'O' : 'X',
-	   (unsigned) die->u.p1.die_hash,
-	   (unsigned) die->u.p1.die_ref_hash, name ? name : "",
-	   get_DW_TAG_name (die->die_tag) + 7);
-  dump_type (die);
+  if (die->die_collapsed_child)
+    {
+      fprintf (stderr, "%*s %x %c", indent, "", die->die_offset,
+	   die->die_ck_state == CK_KNOWN ? 'O' : 'X');
+    }
+  else
+    {
+      const char *name = get_AT_string (die, DW_AT_name);
+      fprintf (stderr, "%*s %x %c %x %x %s %s", indent, "", die->die_offset,
+	       die->die_ck_state == CK_KNOWN ? 'O' : 'X',
+	       (unsigned) die->u.p1.die_hash,
+	       (unsigned) die->u.p1.die_ref_hash, name ? name : "",
+	       get_DW_TAG_name (die->die_tag) + 7);
+      dump_type (die);
+    }
   fprintf (stderr, "\n");
 }
 